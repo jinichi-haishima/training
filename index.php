020984554e331ada4f2f1d9b24b3b94ceea59cf6
jin <haishima.hw@gmail.com>
2026-02-27 22:50:58 +0900

    ヘッダー

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="index.css">
+    <title>Bicycle</title>
+</head>
+<body>
+    <header class="header">
+        <h1>Profile</h1>
+        <li></li>
+        <p>About</p>
+        <p>Bicycle</p>
+    </header>
+
+</body>
+</html>
