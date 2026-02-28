f4a84540dfeecb6128b4e582bc9cb4985e4114cd
jin <haishima.hw@gmail.com>
2026-02-28 20:29:49 +0900

    ひとまず完成

--- index.php
+++ index.php
@@ -5,15 +5,60 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="index.css">
+    <link rel="stylesheet" href="main.css">
     <title>Bicycle</title>
 </head>
 <body>
     <header class="header">
-        <h1>Profile</h1>
-        <li></li>
-        <p>About</p>
-        <p>Bicycle</p>
+        <h1 class="title">Profile</h1>
+        <nav class="header__link">
+            <ul>
+                <li><a href="">About</a></li>
+                <li><a href="">Bicycle</a></li>
+            </ul>
+        </nav>
     </header>
+    <div class="main">
+           <img src="img/img/mainvisual.jpg" alt="Bicycle">
+    </div>
+    <div class="contents">
+        <h2>About</h2>
+        <div class="about">
+            <section class="about__group">
+                <div class="img_prof">
+                    <img src="img/img/about.jpg" alt="">
+                </div>
+                <div class="about__content">
+                    <h3>KAKERU MIYAICHI</h3>
+                    <p>テキストテキストテキストテキストテキストテキストてきすとテキストテキストテキストテキストテキストテキストてきすとテキストテキストテキストテキストテキストテキストてきすと</p>
+                </div>
+            </section>
+        </div>
+        <div class="Bicycle">
+            <h2>Bicycle</h2>
+            <section class="bicycle">
+                <div class="bicycle__1">
+                    <img src="img/img/bicycle1.jpg" alt="">
+                    <h3>タイトルタイトル</h3>
+                    <p>テキストテキストテキスト</p>
+                </div>
+                <div class="bicycle__2">
+                    <img src="img/img/bicycle2.jpg" alt="">
+                    <h3>タイトルタイトル</h3>
+                    <p>テキストテキストテキスト</p>
+                </div>
+                <div class="bicycle__3"> 
+                    <img src="img/img/bicycle3.jpg" alt="">
+                    <h3>タイトルタイトル</h3>
+                    <p>テキストテキストテキスト</p>
+                </div>
+            </section>
 
+        </div>
+
+    </div>
+    <div class="footer">
+        <small>&copy;2020 profile</small>
+    </div>
 </body>
 </html>
